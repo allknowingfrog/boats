f8f4f545bb8f8750d3171918e65200df5acda857
Jeremiah A. Bohling <jeremiah.bohling@gmail.com>
2019-06-21 18:41:22 -0500

    Add empty/not_empty scopes, add seeds

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -1,7 +1,15 @@
 class Shipment < ApplicationRecord
   has_many :containers
 
-  def self.open
+  def self.not_empty
+    joins(:containers)
+  end
+
+  def self.empty
+    left_joins(:containers).where(containers: { id: nil })
+  end
+
+  def self.not_full
     where(id: Shipment
       .joins('LEFT JOIN "containers" ON "containers"."shipment_id" = "shipments"."id"')
       .group('"shipments"."id", "shipments"."capacity", "containers"."shipment_id"')
--- test/models/shipment_test.rb
+++ test/models/shipment_test.rb
@@ -1,24 +1,52 @@
 require 'test_helper'
 
 class ShipmentTest < ActiveSupport::TestCase
-  test "open shipments" do
+  test "not empty" do
     big = Shipment.create(description: "Big", capacity: 3)
     big.containers.create(description: "Things")
     big.containers.create(description: "Stuff")
 
-    medium = Shipment.create(description: "Medium", capacity: 2)
-    medium.containers.create(description: "Whatnot")
+    small = Shipment.create(description: "Small", capacity: 1)
+
+    assert_includes Shipment.not_empty, big
+    assert_not_includes Shipment.not_empty, small
+
+    small.containers.create(description: "Widgets")
+
+    assert_includes Shipment.not_empty, big
+    assert_includes Shipment.not_empty, small
+  end
+
+  test "empty" do
+    big = Shipment.create(description: "Big", capacity: 3)
+    big.containers.create(description: "Things")
+    big.containers.create(description: "Stuff")
 
     small = Shipment.create(description: "Small", capacity: 1)
 
-    assert_includes Shipment.open, big
-    assert_includes Shipment.open, medium
-    assert_includes Shipment.open, small
+    assert_not_includes Shipment.empty, big
+    assert_includes Shipment.empty, small
 
-    medium.containers.create(description: "Widgets")
+    small.containers.create(description: "Widgets")
 
-    assert_includes Shipment.open, big
-    assert_not_includes Shipment.open, medium
-    assert_includes Shipment.open, small
+    assert_not_includes Shipment.empty, big
+    assert_not_includes Shipment.empty, small
+  end
+
+  test "not full" do
+    big = Shipment.create(description: "Big", capacity: 3)
+    big.containers.create(description: "Things")
+    big.containers.create(description: "Stuff")
+
+    small = Shipment.create(description: "Small", capacity: 1)
+
+    assert_includes Shipment.not_full, big
+    assert_includes Shipment.not_full, small
+    assert_equal 2, Shipment.not_full.count
+
+    small.containers.create(description: "Widgets")
+
+    assert_includes Shipment.not_full, big
+    assert_not_includes Shipment.not_full, small
   end
 end
